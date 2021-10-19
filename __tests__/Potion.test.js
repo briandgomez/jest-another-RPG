dbf45b7554268b373be09957df2f27e0a9256d86
briandgomez <briandgomez98@gmail.com>
2021-10-19 19:06:10 -0400

    updated tests

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,23 +1,16 @@
-//Refers to the 'Potion' file
-const Potion = require('../lib/Potion');
+const Potion = require('../lib/Potion.js');
 
-/*test('creates a health potion object', () => {
-    //Creates a new object called 'Potion' with a name of 'health;
+test('creates a health potion object', () => {
     const potion = new Potion('health');
 
-    //Expects name = 'health'
     expect(potion.name).toBe('health');
-    //Expects value = any number
     expect(potion.value).toEqual(expect.any(Number));
-});*/
+});
 
 test('creates a random potion object', () => {
-    //Creates a new object called 'Potion' with no parameter
     const potion = new Potion();
 
-    //Expects name to be a string and exist respectively
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
-    //Expects the value to be equal to any number
     expect(potion.value).toEqual(expect.any(Number));
-})
+});
